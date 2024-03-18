2f98fb904665dd4eabc661173b6b3db0a79383ed
daniyal ali <daniyalali12568@gmail.com>
2024-03-19 02:51:08 +0500

    getdatafrominputfield

--- src/components/Foam.jsx
+++ src/components/Foam.jsx
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Foam = () => {
+    const [user,setUser] = useState({});
+    console.log(user);
+    const getserData= (e)=>{
+        e.preventDefault();
+        setUser({...user , [e.target.name]:e.target.value})
+    }
   return (
     <form className="w-50 mx-auto mt-2">
   <div className="row mb-4">
     <div className="col">
       <div data-mdb-input-init className="form-outline">
-        <input type="text" id="form3Example1" className="form-control border border-dark" />
+        <input onChange={getserData} type="text" name="name" id="form3Example1" className="form-control border border-dark" />
         <label className="form-label" >First name</label>
       </div>
     </div>
     <div className="col">
       <div data-mdb-input-init className="form-outline">
-        <input type="text" id="form3Example2" className="form-control border border-dark" />
+        <input onChange={getserData} type="text" name="lastName" id="form3Example2" className="form-control border border-dark" />
         <label className="form-label" >Last name</label>
       </div>
     </div>
   </div>
 
   <div data-mdb-input-init className="form-outline mb-4">
-    <input type="email" id="form3Example3" className="form-control border border-dark" />
+    <input onChange={getserData} type="email" name="email" id="form3Example3" className="form-control border border-dark" />
     <label className="form-label" >Email address</label>
   </div>
 
   <div data-mdb-input-init className="form-outline mb-4">
-    <input type="text" id="form3Example4" className="form-control border border-dark" />
+    <input onChange={getserData} type="text" name="Age" id="form3Example4" className="form-control border border-dark" />
     <label className="form-label" >Age</label>
   </div>
 
   <div className="form-check d-flex justify-content-start mb-4 ">
-    <input className="form-check-input me-2 " type="checkbox" value="" id="form2Example33"  />
+    <input onChange={getserData} className="form-check-input me-2" name="gender" value="male" type="checkbox"  id="form2Example23"  />
     <label className="form-check-label " >
       Male
     </label>
   </div>
   <div className="form-check d-flex justify-content-start  mb-4">
-    <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33"  />
+    <input onChange={getserData} className="form-check-input me-2" name="gender"  value="female" type="checkbox"  id="form2Example33"  />
     <label className="form-check-label" >
-      Male
+      feMale
     </label>
   </div>
   <button data-mdb-ripple-init type="button" className="btn btn-dark btn-block mb-4">Submit</button>
